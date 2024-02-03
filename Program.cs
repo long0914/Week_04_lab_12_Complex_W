a549ae14bcba309a433ea813bf65e02f17d390e8
long0914 <119887736+long0914@users.noreply.github.com>
2024-02-03 23:05:30 +0000

    Add complex class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text.Json; //add json namespace
+
+namespace Complex
+{
+
+    class Program
+    {
+
+      
+
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+        }
+    }
+}
---
+++ complex.cs
@@ -0,0 +1,12 @@
+namespace Complex{
+
+    public class Complex
+    {
+        public int Real { get;  }
+        public int Imaginary { get;  }
+
+        public double Argument;
+        public double Modulus;
+
+    }
+}
